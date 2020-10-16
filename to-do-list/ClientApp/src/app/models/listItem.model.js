a55809253b525af13144aa9009ab1038363ace5f
Mark Kline <mark.kline1554@hotmail.com>
2020-10-15 19:30:20 -0600

    Starting front-end models

--- to-do-list/ClientApp/src/app/models/listItem.model.js
+++ to-do-list/ClientApp/src/app/models/listItem.model.js
@@ -1,20 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ListItem = void 0;
 var ListItem = /** @class */ (function () {
     function ListItem(obj) {
         this.id = 0;
         this.description = '';
         this.created = new Date;
         this.updated = new Date;
+        this.due = new Date;
         if (obj) {
             this.id = obj.id;
             this.description = obj.description;
+            this.isRecurring = obj.isRecurring;
+            this.frequency = obj.frequency;
+            this.importance = obj.importance;
             this.created = obj.created;
             this.updated = obj.updated;
+            this.due = obj.due;
+            this.completed = obj.completed;
         }
     }
     return ListItem;
 }());
 exports.ListItem = ListItem;
+var Category = /** @class */ (function () {
+    function Category() {
+        this.id = 0;
+        this.name = '';
+    }
+    return Category;
+}());
+exports.Category = Category;
+var Type = /** @class */ (function () {
+    function Type() {
+        this.id = 0;
+        this.name = '';
+    }
+    return Type;
+}());
+exports.Type = Type;
+var Importance;
+(function (Importance) {
+    Importance[Importance["Low"] = 0] = "Low";
+    Importance[Importance["Medium"] = 1] = "Medium";
+    Importance[Importance["High"] = 2] = "High";
+})(Importance || (Importance = {}));
+var Frequency;
+(function (Frequency) {
+    Frequency[Frequency["Daily"] = 0] = "Daily";
+    Frequency[Frequency["Weekly"] = 1] = "Weekly";
+    Frequency[Frequency["BiWeekly"] = 2] = "BiWeekly";
+    Frequency[Frequency["Monthly"] = 3] = "Monthly";
+    Frequency[Frequency["Quarterly"] = 4] = "Quarterly";
+    Frequency[Frequency["Yearly"] = 5] = "Yearly";
+})(Frequency || (Frequency = {}));
 //# sourceMappingURL=listItem.model.js.map
--- to-do-list/ClientApp/src/app/models/listItem.model.ts
+++ to-do-list/ClientApp/src/app/models/listItem.model.ts
@@ -2,9 +2,15 @@ export class ListItem {
   id: number = 0;
 
   description: string = '';
+  isRecurring: boolean;
+
+  frequency: Frequency;
+  importance: Importance;
 
   created: Date = new Date;
   updated: Date = new Date;
+  due: Date = new Date;
+  completed?: Date;
 
   constructor()
   constructor(obj: ListItem)
@@ -13,9 +19,42 @@ export class ListItem {
       this.id = obj.id;
 
       this.description = obj.description;
+      this.isRecurring = obj.isRecurring;
+
+      this.frequency = obj.frequency;
+      this.importance = obj.importance;
 
       this.created = obj.created;
       this.updated = obj.updated;
+      this.due = obj.due;
+      this.completed = obj.completed;
     }
   }
+}
+
+export class Category {
+  id: number = 0;
+
+  name: string = '';
+}
+
+export class Type {
+  id: number = 0;
+
+  name: string = '';
+}
+
+enum Importance {
+  Low,
+  Medium,
+  High
+}
+
+enum Frequency {
+  Daily,
+  Weekly,
+  BiWeekly,
+  Monthly,
+  Quarterly,
+  Yearly
 }
