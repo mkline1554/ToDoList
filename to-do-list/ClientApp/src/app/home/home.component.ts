b620eb8f581b23c75ddb456ab44cd1b78757b1ee
Mark Kline <mark.kline1554@hotmail.com>
2020-10-15 18:46:03 -0600

    basic home layout

--- to-do-list/ClientApp/src/app/home/home.component.ts
+++ to-do-list/ClientApp/src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls:['./home.component.css']
 })
 export class HomeComponent {
 
