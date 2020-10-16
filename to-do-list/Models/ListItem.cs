447bc354c70768a98ab1003cfcc6806613501b8a
Mark Kline <mark.kline1554@hotmail.com>
2020-10-15 19:31:02 -0600

    Starting back-end models

--- to-do-list/Models/ListItem.cs
+++ to-do-list/Models/ListItem.cs
@@ -12,8 +12,18 @@ namespace to_do_list.Models
         public int Id { get; set; }
 
         public string Description { get; set; }
+        public bool IsRecurring { get; set; }
+
+        public Frequency Frequency { get; set; }
+        public Importance Importance { get; set; }
+
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
+        public DateTime Due { get; set; }
+        public DateTime? Completed { get; set; }
+
+        public Category Category { get; set; }
+        public Type Type { get; set; }
 
         public void Update(ListItem obj)
         {
@@ -24,4 +34,38 @@ namespace to_do_list.Models
             }
         }
     }
+}
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class Type
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+
+
+public enum Importance
+{
+    Low,
+    Medium,
+    High
+}
+
+public enum Frequency
+{
+    Daily,
+    Weekly,
+    BiWeekly,
+    Monthly,
+    Quarterly,
+    Yearly
 }
